c3d1f1e26672767558173a6af2dc85af718fa30c
Langat Emmanuel <mannuehkipkirui@gmail.com>
2022-12-16 01:45:17 -0800

    ..

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,11 @@
  *
  * Return: 1 is c if true else 0
 */
-int _isalpha(int c) {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-        return 1;
+int _isalpha(int c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        return (1);
     }
-    return 0;
+    return (0);
 }
