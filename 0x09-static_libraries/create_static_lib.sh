2b1deddff538ec229d0dbe0ee17c669b5e54ed77
Langat Emmanuel <mannuehkipkirui@gmail.com>
2023-01-09 22:09:55 +0300

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-objects=""
-for c_file in *.c; do
-  gcc -c -o "${c_file%.*}.o" "$c_file"
-  objects="$objects ${c_file%.*}.o"
-done
-ar rcs liball.a $objects
+gcc -c *.c
+ar -rc liball.a *.o
